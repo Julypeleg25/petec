ce64491e014b32af0542eaef8dc4d0d6aac7106c
julypeleg25 <julypeleg@gmail.com>
2026-05-03 22:22:38 +0300

    fixed button title in hebrew

--- frontend/src/components/DailyPlan/DailyPlan.constants.ts
+++ frontend/src/components/DailyPlan/DailyPlan.constants.ts
@@ -1,5 +1,5 @@
 export const DAILY_PLAN_PRINT_TITLE = "תכנית יומית";
-export const DAILY_PLAN_PRINT_BUTTON = "הדפס PDF";
+export const DAILY_PLAN_PRINT_BUTTON = "PDF הדפס";
 export const DAILY_PLAN_PRINT_DATE_LABEL = "תאריך הפקה";
 export const DAILY_PLAN_PATIENT_COUNT_LABEL = "מספר מטופלים";
 export const DAILY_PLAN_EMPTY_VALUE = "-";
